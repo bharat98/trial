717e6ec74ebc7c5b7c54c8925b5757ee71173f49
Bharat Gurbaxani <gurbaxanibharat@gmail.com>
2016-11-27 23:57:11 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+char answer ,e;
+cout << " Determined to be consistent ? \n Y or N"<<endl;
+cout << "Enter choice ";
+cin >> answer ;
+if (answer == 'Y' || answer =='y')
+{cout << "Welocome to Git Hub"; }
+else
+{cout << "Not too mant choices everyone gets";}
+return 0;
+}
